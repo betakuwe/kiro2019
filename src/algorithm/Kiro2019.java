84bb17f544a901bae7a9d236cf64c42997fe13eb
betakuwe <33549490+betakuwe@users.noreply.github.com>
2019-11-28 15:09:35 +0100

    update

--- src/algorithm/Kiro2019.java
+++ src/algorithm/Kiro2019.java
@@ -1,5 +1,7 @@
 package algorithm;
 
 public class Kiro2019 {
+  private static double initTemperature = 5000;
+  private static double coolingRate = 1 - Math.exp(-15);
 
 }
--- src/io/GraphReader.java
+++ src/io/GraphReader.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import algorithm.library.AlgoTSP;
-import algorithm.Algorithm;
 
 public class GraphReader {
 
@@ -22,28 +21,13 @@ public class GraphReader {
   public AlgoTSP readInput() {
     try {
       Scanner scanner = new Scanner(new File(filename));
-
-      Graph<Edge> g = new Graph<>(NUM_VERTEX);
-      
-      int i = 0;
-      int j = 0;
-      for(int k = 0; k < NUM_EDGE; k++) {
         // ==================================================
         // todo write input here
-    	  int distance = Integer.parseInt(scanner.next());
-    	  if(i != j) {
-    		  g.setDirectedEdge(i, j, new Edge(distance));
-    	  }
-    	  j++;
-    	  if(j % NUM_VERTEX == 0) {
-    		  i++;
-    		  j = 0;
-    	  }
-      }
+
       
       scanner.close();
       
-      return new AlgoTSP(g); // todo
+      return null; // todo
       // ==================================================
     } catch (FileNotFoundException e) {
       e.printStackTrace();
